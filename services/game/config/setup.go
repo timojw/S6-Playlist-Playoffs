42f48f515f28ab2ac04cbe43fdfca5cc04ae5f0f
timojw <82544433+timojw@users.noreply.github.com>
2024-06-28 09:20:47 +0200

    d

--- services/game/config/setup.go
+++ services/game/config/setup.go
@@ -1,53 +1,53 @@
-package configs
+// package configs
 
-import (
-	"context"
-	"fmt"
-	"log"
-	"time"
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
+// 	"go.mongodb.org/mongo-driver/bson"
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
 
-// ConnectDB connects to the MongoDB database
-func ConnectDB() *mongo.Client {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel() // Call the cancel function to avoid a context leak
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
-	if err != nil {
-		log.Fatal(err)
-	}
+// // ConnectDB connects to the MongoDB database
+// func ConnectDB() *mongo.Client {
+// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+// 	defer cancel() // Call the cancel function to avoid a context leak
+// 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(EnvMongoURI()))
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	//ping the database
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+// 	//ping the database
+// 	err = client.Ping(ctx, nil)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
 
-	fmt.Println("Connected to MongoDB")
+// 	fmt.Println("Connected to MongoDB")
 
-	return client
-}
+// 	return client
+// }
 
-// Client instance
-var DB *mongo.Client = ConnectDB()
+// // Client instance
+// var DB *mongo.Client = ConnectDB()
 
-// getting database collections
-func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	collection := client.Database("games-db").Collection(collectionName)
-	return collection
-}
+// // getting database collections
+// func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+// 	collection := client.Database("games-db").Collection(collectionName)
+// 	return collection
+// }
 
-func SetIndexes() {
-	gameCollection := GetCollection(DB, "games")
-	indexModel := mongo.IndexModel{
-		Keys:    bson.M{"name": 1},
-		Options: options.Index().SetUnique(true),
-	}
-	_, err := gameCollection.Indexes().CreateOne(context.Background(), indexModel)
-	if err != nil {
-		panic(err)
-	}
-}
+// func SetIndexes() {
+// 	gameCollection := GetCollection(DB, "games")
+// 	indexModel := mongo.IndexModel{
+// 		Keys:    bson.M{"name": 1},
+// 		Options: options.Index().SetUnique(true),
+// 	}
+// 	_, err := gameCollection.Indexes().CreateOne(context.Background(), indexModel)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
